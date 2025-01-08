7105aaebc4332d4a3c369effa4a7442f84b107ce
Hugo LORENT <hugo.lorent@outlook.com>
2025-01-08 16:26:58 +0100

    improve catclone, read stdin when program has no args

--- src/bin/catclone.rs
+++ src/bin/catclone.rs
@@ -2,10 +2,11 @@ use anyhow::{Context, Result};
 use clap::Parser;
 use std::io::{BufRead, BufReader};
 
-/// Concatenate FILE[s] to standard output.
+/// Concatenate FILE(s) to standard output. Read standard input when no FILE is given
 #[derive(Parser)]
 struct Cli {
     /// The paths of the files to concatenate
+    #[arg(value_name = "FILE")]
     files: Vec<std::path::PathBuf>,
 }
 
@@ -13,18 +14,27 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
     let mut result = String::new();
 
-    for file_path in &args.files {
-        let file = std::fs::File::open(file_path)
-            .with_context(|| format!("could not read file `{}`", file_path.display()))?;
-        let reader = BufReader::new(file);
-
-        for line in reader.lines() {
-            let line = line?;
-            result.push_str(&line);
-            result.push('\n');
+    if args.files.len() >= 1 {
+        for file_path in &args.files {
+            let file = std::fs::File::open(file_path)
+                .with_context(|| format!("could not read file `{}`", file_path.display()))?;
+            let reader = BufReader::new(file);
+            read_input(reader, &mut result)?;
         }
+    } else {
+        let reader = BufReader::new(std::io::stdin().lock());
+        read_input(reader, &mut result)?;
     }
 
-    print!("{}", result);
+    print!("{result}");
+    Ok(())
+}
+
+fn read_input<R: BufRead>(reader: R, result: &mut String) -> Result<(), std::io::Error> {
+    for line in reader.lines() {
+        let line = line?;
+        result.push_str(&line);
+        result.push('\n');
+    }
     Ok(())
 }
