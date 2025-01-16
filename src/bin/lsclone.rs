80cd17006c3e340148eee98099c7827d9e60c3ee
Hugo LORENT <hugo.lorent@outlook.com>
2025-01-16 13:57:23 +0100

    improve lsclone, add -l option

--- src/bin/lsclone.rs
+++ src/bin/lsclone.rs
@@ -1,3 +1,5 @@
+use std::os::unix::fs::{MetadataExt, PermissionsExt};
+
 use clap::Parser;
 use colored::Colorize;
 
@@ -10,18 +12,106 @@ struct Cli {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let _args = Cli::parse();
+    let args = Cli::parse();
     let dir_entry = std::fs::read_dir(".")?;
 
     for entry in dir_entry {
         let entry = entry?;
-        if entry.file_type()?.is_dir() {
-            println!("{}  ", entry.file_name().to_string_lossy().blue().bold());
-        } else if entry.file_type()?.is_symlink() {
-            println!("{}  ", entry.file_name().to_string_lossy().red().bold());
-        } else {
-            println!("{}  ", entry.file_name().to_string_lossy());
-        }
+        print_entry(args.long, entry)?;
+    }
+
+    Ok(())
+}
+
+fn print_entry(long: bool, entry: std::fs::DirEntry) -> Result<(), Box<dyn std::error::Error>> {
+    let file_name = entry.file_name();
+
+    let file_name = match entry.file_type()? {
+        ft if ft.is_dir() => file_name.to_string_lossy().blue().bold(),
+        ft if ft.is_symlink() => file_name.to_string_lossy().red().bold(),
+        _ => file_name.to_string_lossy().bright_white(),
+    };
+
+    if long {
+        let metadata = entry.metadata()?;
+        let perms = metadata.permissions().mode();
+        let perms_str = permissions_to_string(perms);
+        let uid = metadata.uid();
+        let gid = metadata.gid();
+        let size = metadata.size();
+        println!("{} {} {} {} {}", perms_str, uid, gid, size, file_name);
+    } else {
+        println!("{}  ", file_name);
     }
     Ok(())
+}
+
+fn permissions_to_string(mode: u32) -> String {
+    let mut perms = String::new();
+
+    // Type de fichier
+    if mode & 0o40000 == 0o40000 {
+        perms.push('d'); // dossier
+    } else if mode & 0o120000 == 0o120000 {
+        perms.push('l'); // lien symbolique
+    } else if mode & 0o140000 == 0o140000 {
+        perms.push('b'); // bloc spécial
+    } else if mode & 0o150000 == 0o150000 {
+        perms.push('c'); // caractère spécial
+    } else {
+        perms.push('-'); // fichier régulier
+    }
+
+    // Permissions utilisateur
+    if mode & 0o400 == 0o400 {
+        perms.push('r');
+    } else {
+        perms.push('-');
+    }
+    if mode & 0o200 == 0o200 {
+        perms.push('w');
+    } else {
+        perms.push('-');
+    }
+    if mode & 0o100 == 0o100 {
+        perms.push('x');
+    } else {
+        perms.push('-');
+    }
+
+    // Permissions groupe
+    if mode & 0o040 == 0o040 {
+        perms.push('r');
+    } else {
+        perms.push('-');
+    }
+    if mode & 0o020 == 0o020 {
+        perms.push('w');
+    } else {
+        perms.push('-');
+    }
+    if mode & 0o010 == 0o010 {
+        perms.push('x');
+    } else {
+        perms.push('-');
+    }
+
+    // Permissions autres
+    if mode & 0o004 == 0o004 {
+        perms.push('r');
+    } else {
+        perms.push('-');
+    }
+    if mode & 0o002 == 0o002 {
+        perms.push('w');
+    } else {
+        perms.push('-');
+    }
+    if mode & 0o001 == 0o001 {
+        perms.push('x');
+    } else {
+        perms.push('-');
+    }
+
+    perms
 }
